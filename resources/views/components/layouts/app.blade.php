36ea03c4ed21f46e90d6a683b02772301b7308a9
niel2512 <nielyusuf17@gmail.com>
2025-06-13 19:09:47 +0700

    Build Main Layout Products Using Livewire

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,9 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ isset($title) ? "$title - " . config('app.name') : config('app.name') }}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+        <link rel="manifest" href="/site.webmanifest">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @stack('head')
     </head>
-    <body>
+    <body class="dark:bg-neutral-800 dark:border-neutral-700">
+        <x-navigation />
         {{ $slot }}
+        <x-footer />
     </body>
 </html>
