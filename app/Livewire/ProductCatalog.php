c78a574218fc80480049f304a726f6dbb0ed8937
niel2512 <nielyusuf17@gmail.com>
2025-06-14 15:55:29 +0700

    Activate The Product Catalog Search, Filter & Sorting

--- app/Livewire/ProductCatalog.php
+++ app/Livewire/ProductCatalog.php
@@ -8,16 +8,76 @@ use App\Data\ProductData;
 use App\Models\Product;
 use App\Models\Tag;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ProductCatalog extends Component
 {
+    use WithPagination;
+
+    // queryString agar di link nya terdapat search= ketika dilakukan search
+    public $queryString = [
+        'select_collections' => ['except' => []],
+        'search' => ['except' => []],
+        'sort_by' => ['except' => 'newest']
+    ];
+
+    public array $select_collections = [];
+
+    public string $search = '';
+
+    public string $sort_by = 'newest'; //latest, price_asc, price_desc
+
+    public function applyFilters()
+    {
+        // dd($this->select_collections, $this->search, $this->sort_by);
+        $this->resetPage(); //resetPage komponen bawaan livewire diambil dari WithPagination
+    }
+
+    public function resetFilters()
+    {
+        $this->select_collections = [];
+        $this->search = '';
+        $this->sort_by = 'newest';
+        $this->resetPage();
+    }
+
     public function render()
     {
         $collection_result = Tag::query()->withType('collection')->withCount('Products')->get();
-        $query = Product::paginate(3); //ORM hanya bertugas konek ke db
+        // $query = Product::paginate(3); //ORM hanya bertugas konek ke db
+        
+        // Query untuk menampilkan link search= ketika user melakukan filter
+        $query = Product::query();
+        if ($this->search) {
+            $query->where('name', 'LIKE', "%{$this->search}%");
+            // dd($query->get());  
+        }
+
+        // Filtering untuk Select Collections
+        if (!empty($this->select_collections)) {
+            $query->whereHas('tags', function($query){
+                $query->whereIn('id', $this->select_collections);
+            });
+        }
+
+        // Filtering untuk Sort By
+        switch($this->sort_by) {
+            case 'latest':
+                $query->oldest();
+                break;
+            case 'price_asc':
+                $query->orderBy('price', 'asc');
+                break;
+            case 'price_desc':
+                $query->orderBy('price', 'desc');
+            deafult:
+                $query->latest();
+                break;
+        }
         
         // DTO yang hanya mempassing data tanpa konek ke database
-        $products = ProductData::collect($query);
+        // $products = ProductData::collect($query);
+        $products = ProductData::collect($query->paginate(9));
         $collections = ProductCollectionData::collect($collection_result);
         return view('livewire.product-catalog', compact('products', 'collections'));
     }
