68d0d67b1e9fef97da9e260e40e0fa74f52df1c1
niel2512 <nielyusuf17@gmail.com>
2025-06-17 18:17:55 +0700

    Create State for Sales Order Status Transition

--- app/States/SalesOrder/SalesOrderState.php
+++ app/States/SalesOrder/SalesOrderState.php
@@ -1,21 +1,25 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\States\SalesOrder;
 
+use App\States\SalesOrder\Transitions\PendingToCancel;
+use App\States\SalesOrder\Transitions\PendingToProgress;
+use App\States\SalesOrder\Transitions\ProgressToCompleted;
 use Spatie\ModelStates\State;
 use Spatie\ModelStates\StateConfig;
 
 abstract class SalesOrderState extends State
 {
-  abstract public function label() : string;
+  abstract public function label(): string;
 
-  public static function config() : StateConfig
+  public static function config(): StateConfig
   {
     return parent::config()
       ->default(Pending::class)
-      ->allowTransition(Pending::class, Progress::class)
-      ->allowTransition(Pending::class, Cancel::class)
-      ->allowTransition(Progress::class, Completed::class);
+      ->allowTransition(Pending::class, Progress::class, PendingToProgress::class)
+      ->allowTransition(Pending::class, Cancel::class, PendingToCancel::class)
+      ->allowTransition(Progress::class, Completed::class, ProgressToCompleted::class);
   }
 }
